0d83e8dd4cfa89352d69068c46c1116f7556c669
LyceenAiro <1534012710@qq.com>
2023-09-18 21:26:12 +0800

    清理一些旧文件

--- utli/dir.py
+++ utli/dir.py
@@ -1,6 +1,9 @@
 from os import path
 import sys
 
+from botpy import logging
+_log = logging.get_logger()
+
 # The very initial message
 print('More information at https://github.com/LyceenAiro/Saccharomyces-cerevisiae-Fix')
 
@@ -8,6 +11,6 @@ TEST_MODE = 1
 # Turn off test mode when packing the program
 if TEST_MODE:
     local_dir = '/'.join(path.dirname(path.abspath(__file__)).replace('\\', '/').split('/')[:-1])
+    _log.info('TEST_MODE on') if TEST_MODE else None
 else:
     local_dir = path.dirname(path.abspath(sys.executable)).replace('\\', '/')
-    print(11)
--- utli/logger.py
+++
@@ -1,6 +0,0 @@
-from botpy import logging
-_log = logging.get_logger()
-
-from .dir import local_dir, TEST_MODE
-
-_log.info('TEST_MODE on') if TEST_MODE else None
